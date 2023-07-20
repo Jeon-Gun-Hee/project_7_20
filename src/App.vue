7758d4b94977146c8226a68ca7753967ebac793e
Gunhee Jeon <rjsgml420@gmail.com>
2023-07-20 15:39:27 +0900

    todaycommit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
+  <div class="menu">
+   <a>오늘 뭐먹지?</a>
+    
+  </div>
+
+ 
+  
+ 
+  
+  
+
+  <div class="bottom_menu">
+  <a>image</a>
+  <a>image2</a>
+  <a>image3</a>
+  
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      메뉴: ['home', 'shop', 'about'],
+      price1 : 60,
+      price2 : 70,
+      products :['역삼동원룸' ,'천호동원룸','마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
 <style>
 #app {
+  background: rgb(255, 234, 221);
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 20px;
+  color: black;
+  position: fixed;
+  bottom: 0;
+  left:0;
+  right: 0;
+  top: 0;
 }
+
+.menu {
+  background: rgb(255, 137, 137);
+  padding: 30px;
+  border-radius: 5px;
+  text-align: center;
+}
+.menu a {
+  color: white;
+  padding: 10px;
+  
+}
+
+.bottom_menu {
+  background:rgb(255, 137, 137);
+  padding: 30px;
+  border-radius: 5px;
+  position: fixed;
+  bottom: 0;
+  left:0;
+  right: 0;
+}
+.bottom_menu a {
+  color: white;
+  padding: 100px;
+}
+body {
+  margin :0px;
+}
+
+
 </style>
